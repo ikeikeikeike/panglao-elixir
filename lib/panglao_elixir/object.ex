01777062d96aff73f8a8e31bc037257eaf572079
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-04-13 20:35:25 +0900

    added object functions

--- lib/panglao_elixir/object.ex
+++ lib/panglao_elixir/object.ex
@@ -11,10 +11,37 @@ defmodule PanglaoElixir.Object do
   get "/splash", ObjectController, :splash
   """
 
+  def link!(token, params) do
+    get! "/object/link", auth(token), params:  params
+  end
+  def link!(params) do
+    link! Auth.token, params
+  end
+
+  def info!(token, params) do
+    get! "/object/info", auth(token), params:  params
+  end
+  def info!(params) do
+    info! Auth.token, params
+  end
+
+  def rename!(token, params) do
+    get! "/object/rename", auth(token), params:  params
+  end
+  def rename!(params) do
+    rename! Auth.token, params
+  end
+
+  def upload!(token, params) do
+    get! "/object/upload", auth(token), params:  params
+  end
+  def upload!(params) do
+    upload! Auth.token, params
+  end
+
   def splash!(token, params) do
     get! "/object/splash", auth(token), params:  params
   end
-
   def splash!(params) do
     splash! Auth.token, params
   end
