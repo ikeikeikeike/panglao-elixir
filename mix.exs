01be0007fa2261cc93c944099e548aae1ad268b2
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-07-05 12:29:01 +0900

    aa

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PanglaoElixir.Mixfile do
   def project do
     [app: :panglao_elixir,
      version: "0.1.7",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
